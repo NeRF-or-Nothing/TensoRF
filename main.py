1376312a98daa8bff42be5c1cb1c3721175b6a84
simon <stgrb2002@gmail.com>
2024-03-12 16:53:25 -0400

    Initial nerf worker running test

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import json
 import time
 from multiprocessing import Process, connection
 import os
+from dataLoader import sfm2nerf
+from opt import config_parser
+from worker import train_tensorf
 
 app = Flask(__name__)
 #base_url = "http://localhost:5200/"
@@ -26,33 +29,54 @@ def nerf_worker():
     )
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.queue_declare(queue = 'nerf_in')
-    channel.queue_declare(queue = 'nerf_out')
+    channel.queue_declare(queue = 'nerf-in')
+    channel.queue_declare(queue = 'nerf-out')
     print("DEBUG: Connected to RabbitMQ", flush=True)
 
     def process_nerf_worker(ch, method, properties, body):
 
         job_data = json.loads(body.decode())
         print(f"DEBUG: Running New Job With ID: {job_data['id']}")
-        print("DEBUG: job_data", job_data, flush=True)
-      
-        if True:
-          print("Starting New Nerf Job!")
-          return
+        print("DEBUG: job_data = ", job_data, flush=True)
+        print("Starting New Nerf Job!")
 
         #TODO: not sure what data is needed for nerf to run
-        nerf_data = json.load(body)
-        id = nerf_data["id"]
+        # get data from rabbitmq
+        nerf_data = json.loads(body)
+        id = nerf_data["id"]  
         width = nerf_data["vid_width"]
         height = nerf_data["vid_height"]
         intrinsic_matrix = nerf_data["intrinsic_matrix"]
         frames = nerf_data["frames"]
-        for i in frames:
-            url = requests.get(frames["file_path"])
+        jobdir = Path(f"data/sfm_data/{id}")
+        if not jobdir.exists():
+          os.makedirs(jobdir)
+        print(f"DEBUG: len(frames)={len(frames)}", flush=True)
+        
+        for i, fr_ in enumerate(frames):
+            # Save copy of motion data
+            url = fr_["file_path"]
+            print(f"DEBUG: url={url}", flush=True)
+            
+            # TODO: Add compatibility for both docker and local 
+            url = url.replace("host.docker.internal", "0.0.0.0")
+            print(f"DEBUG: url2{url}", flush=True)
+            img = requests.get(url)
+            # save img to jobdir and replace old file path with local file path
+            fr_["file_path"] = f"{i}.png"
+            img_file_path = jobdir / fr_["file_path"]
+            img_file_path.write_bytes(img.content)
 
-        #TODO: call nerf to do smthing
-        nerf_vid_filepath = nerf()
-        rendered_vid = requests.get(nerf_vid_filepath)
+        jobdir2 = jobdir / f"transforms_train.json"
+        jobdir2.write_text(json.dumps(nerf_data, indent=4))
+        args = config_parser("--config configs/localworkerconfig.txt")
+        args.datadir += f"/{id}"
+        args.expname = id
+        print(f"DEBUG: args={args}", flush=True)
+        logfolder, tensorf_model = train_tensorf(args)
+        
+        return
+        #TODO: get nerf video and submit to rabbitmq
 
 
         #create dict for output video and filepath
@@ -66,6 +90,7 @@ def nerf_worker():
     channel.basic_consume(queue='nerf-in', on_message_callback=process_nerf_worker, auto_ack=False)
     channel.start_consuming()
     # Should not get here!
+
 
 def start_flask():
     global app
